f8437988774fb6b1e95936e9933338bda22ef8d9
s7d11 <sebastcode@outlook.com>
2022-09-20 08:23:18 -0400

    wxdependencies removal

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
         'intervaltree',
         'pytrie',
         'regex',
-        'wxPython',
+        #'wxPython',
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
